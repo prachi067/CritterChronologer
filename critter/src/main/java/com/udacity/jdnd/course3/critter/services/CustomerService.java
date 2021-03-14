60598d207ae69260d6555576168da3cf686fc069
prachjai <prachjai@adobe.com>
2021-03-15 00:53:03 +0530

    Added Transactional annotation

--- critter/src/main/java/com/udacity/jdnd/course3/critter/services/CustomerService.java
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/services/CustomerService.java
@@ -6,9 +6,11 @@ import com.udacity.jdnd.course3.critter.repositories.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 public class CustomerService {
     @Autowired
     CustomerRepository customerRepository;
